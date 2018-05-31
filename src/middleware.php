d07ca4e9d743ede25803d6360777fd1c009777dc
Gilson <gilsonn_souza@hotmail.com>
2018-05-30 23:34:13 -0300

    basic auth

--- src/middleware.php
+++ src/middleware.php
@@ -2,3 +2,23 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+
+/**
+ * Auth básica HTTP
+ */
+$app->add(new \Tuupola\Middleware\HttpBasicAuthentication([
+    /**
+     * Usuários existentes
+     */
+    "users" => [
+        "root" => "toor"
+    ],
+    /**
+     * Blacklist - Deixa todas liberadas e só protege as dentro do array
+     */
+    "path" => ["/auth"],
+    /**
+     * Whitelist - Protege todas as rotas e só libera as de dentro do array
+     */
+    //"passthrough" => ["/auth/liberada", "/admin/ping"],
+]));
--- src/routes.php
+++ src/routes.php
@@ -5,6 +5,16 @@ use Slim\Http\Response;
 
 // Routes
 
+
+/**
+ * HTTP Auth - Autenticação minimalista para retornar um JWT
+ */
+$app->get('/auth', function (Request $request, Response $response) use ($app) {
+    return $response->withJson(["status" => "Autenticado!"], 200)
+        ->withHeader('Content-type', 'application/json');
+});
+
+
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
